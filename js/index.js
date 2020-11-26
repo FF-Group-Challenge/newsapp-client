042a25e0f31b8a223cfa63b72d5835fba8c02e9c
andhikam <andhikamuttaqien@gmail.com>
2020-11-27 00:59:31 +0700

    Add Global & Space News Fetch Features

--- js/index.js
+++ js/index.js
@@ -20,11 +20,11 @@ function nationalNews() {
   $('#content').empty()
 
   $.getJSON('/example/cnn.json', function (data) {
-    console.log(data.data);
+    // console.log(data.data);
 		const news = data.data;
 		for (let i = 0; i < news.length; i++) {
-			console.log(news[i]);
-			$('#content').append(`<br><div class="card" style="width: 18rem;">
+			// console.log(news[i]);
+			$('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
 						<img class="card-img-top" src="${news[i].poster}" alt="Card image cap">
 						<div class="card-body">
 							<h5 class="card-title"></h5>
@@ -35,5 +35,47 @@ function nationalNews() {
 						</div>
 					</div>`);
 		}
+	});
+}
+
+function globalNews() {
+  $('#content').empty()
+
+  $.getJSON('/example/global.json', function (data) {
+    console.log(data.articles);
+		for(let i = 0; i < data.articles.length; i++) {
+      const news = data.articles[i]
+      $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
+						<img class="card-img-top" src="${news.urlToImage}" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">${news.title}</h5>
+							<p class="card-text">${news.description}</p>
+						</div>
+						<div class="card-body">
+							<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+						</div>
+					</div>`);
+    }
+	});
+}
+
+function spaceNews() {
+  $('#content').empty()
+
+  $.getJSON('/example/space.json', function (data) {
+    console.log(data);
+		for(let i = 0; i < data.length; i++) {
+      const news = data[i]
+      $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
+						<img class="card-img-top" src="${news.imageUrl}" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title">${news.title}</h5>
+							<p class="card-text">${news.summary}</p>
+						</div>
+						<div class="card-body">
+							<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+						</div>
+					</div>`);
+    }
 	});
 }

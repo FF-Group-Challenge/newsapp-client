fd9903c1f4a33b0c0cafd5d1a6cdf2bda2adeaa8
andhikam <andhikamuttaqien@gmail.com>
2020-11-27 10:01:23 +0700

    Connecting Client to Server for Fetching Data News

--- js/index.js
+++ js/index.js
@@ -20,31 +20,84 @@ function nationalNews() {
 	$('#content').empty()
 	$("#content_title").empty()
 	$('#content_title').append(`<h1>What's Happening in Indonesia?</h1><br>`)
-  $.getJSON('/example/cnn.json', function (data) {
-    // console.log(data.data);
+	// -- DUMMY FETCHING --
+  // $.getJSON('/example/cnn.json', function (data) {
+  //   console.log(data.data);
+	// 	const news = data.data;
+	// 	for (let i = 0; i < news.length; i++) {
+	// 		// console.log(news[i]);
+	// 		$('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
+	// 					<img class="card-img-top" src="${news[i].poster}" alt="Card image cap">
+	// 					<div class="card-body">
+	// 						<h5 class="card-title">${news[i].judul}</h5>
+	// 						<p class="card-text">${news[i].waktu}</p>
+	// 					</div>
+	// 					<div class="card-body">
+	// 						<a href="${news[i].link}" class="card-link">Info Lebih Lanjut</a>
+	// 					</div>
+	// 				</div>`);
+	// 	}
+	// });
+	$.ajax({
+		url: 'http://localhost:3000/news-indonesia',
+		method: 'GET',
+		headers: {
+			access_token: localStorage.getItem('access_token')
+		}
+	})
+	.done((data) => {
+		console.log(data)
 		const news = data.data;
 		for (let i = 0; i < news.length; i++) {
 			// console.log(news[i]);
 			$('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
 						<img class="card-img-top" src="${news[i].poster}" alt="Card image cap">
 						<div class="card-body">
-							<h5 class="card-title"></h5>
-							<p class="card-text">${news[i].judul}</p>
+							<h5 class="card-title">${news[i].judul}</h5>
+							<p class="card-text">${news[i].waktu}</p>
 						</div>
 						<div class="card-body">
-							<a href="${news[i].link}" class="card-link">Info Lebih Lanjut</a>
+							<a href="${news[i].link}" class="card-link">For further info, click this link</a>
 						</div>
 					</div>`);
 		}
-	});
+
+	})
+	.fail((err) => {
+		console.log(err)
+	})
 }
 
 function globalNews() {
 	$('#content').empty()
 	$("#content_title").empty()
 	$('#content_title').append(`<h1>What's Happening in Our Earth?</h1><br>`)
-  $.getJSON('/example/global.json', function (data) {
-    console.log(data.articles);
+	// -- DUMMY FETCHING --
+  // $.getJSON('/example/global.json', function (data) {
+  //   console.log(data.articles);
+	// 	for(let i = 0; i < data.articles.length; i++) {
+  //     const news = data.articles[i]
+  //     $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
+	// 					<img class="card-img-top" src="${news.urlToImage}" alt="Card image cap">
+	// 					<div class="card-body">
+	// 						<h5 class="card-title">${news.title}</h5>
+	// 						<p class="card-text">${news.description}</p>
+	// 					</div>
+	// 					<div class="card-body">
+	// 						<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+	// 					</div>
+	// 				</div>`);
+  //   }
+	// });
+	$.ajax({
+		url: 'http://localhost:3000/news-global',
+		method: 'GET',
+		headers: {
+			access_token: localStorage.getItem('access_token')
+		}
+	})
+	.done((data) => {
+		console.log(data.articles);
 		for(let i = 0; i < data.articles.length; i++) {
       const news = data.articles[i]
       $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
@@ -54,33 +107,63 @@ function globalNews() {
 							<p class="card-text">${news.description}</p>
 						</div>
 						<div class="card-body">
-							<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+							<a href="${news.url}" class="card-link">For further info, click this link</a>
 						</div>
 					</div>`);
     }
-	});
+	})
+	.fail((err) => {
+		console.log(err)
+	})
 }
 
 function spaceNews() {
 	$('#content').empty()
 	$("#content_title").empty()
 	$('#content_title').append(`<h1>What's Happening in Our Space?</h1><br>`)
-  $.getJSON('/example/space.json', function (data) {
-    console.log(data);
+	// -- DUMMY FETCHING --
+  // $.getJSON('/example/space.json', function (data) {
+  //   console.log(data);
+	// 	for(let i = 0; i < data.length; i++) {
+  //     const news = data[i]
+  //     $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
+	// 					<img class="card-img-top" src="${news.imageUrl}" alt="Card image cap">
+	// 					<div class="card-body">
+	// 						<h5 class="card-title">${news.title}</h5>
+	// 						<p class="card-text">${news.summary}</p>
+	// 					</div>
+	// 					<div class="card-body">
+	// 						<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+	// 					</div>
+	// 				</div>`);
+  //   }
+	// });
+	$.ajax({
+		url: 'http://localhost:3000/news-space',
+		method: 'GET',
+		headers: {
+			access_token: localStorage.getItem('access_token')
+		}
+	})
+	.done((data) => {
+		console.log(data);
 		for(let i = 0; i < data.length; i++) {
       const news = data[i]
       $('#content').append(`<br><div class="card mr-1 ml-1 mt-1 mb-1" style="width: 16rem;">
 						<img class="card-img-top" src="${news.imageUrl}" alt="Card image cap">
 						<div class="card-body">
 							<h5 class="card-title">${news.title}</h5>
-							<p class="card-text">${news.summary}</p>
+							<p class="card-text">${news.summary ? news.summary : "<p class='small'>*No Summary, please click link for further info</p>"}</p>
 						</div>
 						<div class="card-body">
-							<a href="${news.url}" class="card-link">Info Lebih Lanjut</a>
+							<a href="${news.url}" class="card-link">For further info, click this link</a>
 						</div>
 					</div>`);
     }
-	});
+	})
+	.fail((err) => {
+		console.log(err)
+	})
 }
 
 function logout() {
